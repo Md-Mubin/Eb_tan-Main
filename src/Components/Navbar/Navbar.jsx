44ae783894c43a3a8b673a7ddb9e85f65c7ae171
Md Mubin <fontdevmubin@gmail.com>
2024-10-16 23:26:54 +0600

    new file:   src/Components/Navbar/Navbar.css 	modified:   src/Components/Navbar/Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 // ===================== All Imports
-import { Link }              from 'react-router-dom'
-import React, { useState }   from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import { HiBars3BottomLeft } from 'react-icons/hi2'
 
 const Navbar = () => {
@@ -8,9 +9,16 @@ const Navbar = () => {
     // for toggle method
     const [navItems, setNavItems] = useState(false)
 
+    // const [reload, setReload] = useState(true)
+
     const toggleNav = () => {
         setNavItems(!navItems)
+        // setReload(true)
     }
+
+    // useEffect(()=>{
+    //     setReload(false)
+    // },[])
 
     return (
         <>
@@ -23,17 +31,17 @@ const Navbar = () => {
 
                     <li className='relative'>
 
-                        {/* --------- bar icon --------- */}
+                        {/* --------- bar icon button --------- */}
                         <HiBars3BottomLeft onClick={toggleNav} className={navItems ? 'text-4xl cursor-pointer rotate-[-360deg] duration-500' : 'text-4xl rotate-180 duration-500 cursor-pointer'} />
 
                         {/* toggle effect */}
-                        <ul className={navItems ? "w-[150px] flex flex-col text-center gap-3 py-4 bg-[#ff9d9da8] absolute translate-x-[-120px] duration-500 opacity-1 rounded-2xl" : "w-[150px] flex flex-col text-center gap-3 py-4 bg-red-200 absolute translate-x-[50px] duration-500 opacity-0 pointer-events-none"}>
-
-                            <Link to = "#" className='font-inter font-normal hover:font-semibold 2xl:py-2 w-full hover:bg-red-300 hover:text-white duration-200 rounded-2xl'>Home</Link>
-                            <Link to = "#" className='font-inter font-normal hover:font-semibold 2xl:py-2 w-full hover:bg-red-300 hover:text-white duration-200 rounded-2xl'>About us</Link>
-                            <Link to = "#" className='font-inter font-normal hover:font-semibold 2xl:py-2 w-full hover:bg-red-300 hover:text-white duration-200 rounded-2xl'>Product</Link>
-                            <Link to = "#" className='font-inter font-normal hover:font-semibold 2xl:py-2 w-full hover:bg-red-300 hover:text-white duration-200 rounded-2xl'>Contact</Link>
-                        </ul>
+                            <ul className={`w-[150px] flex flex-col text-center gap-3 py-4 bg-[#ff9d9da8] absolute rounded-2xl ${navItems ? "slideIn" : "slideOut"}`}>
+                                <Link to="#" className='font-inter font-normal hover:font-semibold 2xl:py-2 w-full hover:bg-red-300 hover:text-white duration-200 rounded-2xl'>Home</Link>
+                                <Link to="#" className='font-inter font-normal hover:font-semibold 2xl:py-2 w-full hover:bg-red-300 hover:text-white duration-200 rounded-2xl'>About us</Link>
+                                <Link to="#" className='font-inter font-normal hover:font-semibold 2xl:py-2 w-full hover:bg-red-300 hover:text-white duration-200 rounded-2xl'>Product</Link>
+                                <Link to="#" className='font-inter font-normal hover:font-semibold 2xl:py-2 w-full hover:bg-red-300 hover:text-white duration-200 rounded-2xl'>Contact</Link>
+                            </ul>
+                        
                     </li>
                 </ul>
             </nav>
