59e018f1489ac8d1575c5bc83f59133855dd7a58
Md Mubin <fontdevmubin@gmail.com>
2024-10-16 22:38:04 +0600

    modified:   src/Components/Navbar/Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
                         <HiBars3BottomLeft onClick={toggleNav} className={navItems ? 'text-4xl cursor-pointer rotate-[-360deg] duration-500' : 'text-4xl rotate-180 duration-500 cursor-pointer'} />
 
                         {/* toggle effect */}
-                        <ul className={navItems ? "w-[150px] flex flex-col items-center gap-3 py-4 bg-[#ff9d9da8] absolute translate-x-[-120px] duration-500 opacity-1 rounded-2xl" : "w-[150px] flex flex-col items-center gap-3 py-4 bg-red-200 absolute translate-x-[50px] duration-500 opacity-0 pointer-events-none"}>
+                        <ul className={navItems ? "w-[150px] flex flex-col text-center gap-3 py-4 bg-[#ff9d9da8] absolute translate-x-[-120px] duration-500 opacity-1 rounded-2xl" : "w-[150px] flex flex-col text-center gap-3 py-4 bg-red-200 absolute translate-x-[50px] duration-500 opacity-0 pointer-events-none"}>
 
                             <Link to = "#" className='font-inter font-normal hover:font-semibold 2xl:py-2 w-full hover:bg-red-300 hover:text-white duration-200 rounded-2xl'>Home</Link>
                             <Link to = "#" className='font-inter font-normal hover:font-semibold 2xl:py-2 w-full hover:bg-red-300 hover:text-white duration-200 rounded-2xl'>About us</Link>
